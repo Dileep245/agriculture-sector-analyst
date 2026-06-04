2a3489b885c81c6ee883667b1e3d93ae4ff597b5
Dileep245 <dileepks615@gmail.com>
2026-06-04 15:32:39 +0530

    Update analytics.py

--- utils/analytics.py
+++ utils/analytics.py
@@ -1,7 +1,23 @@
-def clean_data(df):
+def total_production(df):
 
-    df.drop_duplicates(inplace=True)
+    return df["Production"].sum()
 
-    df.fillna(0, inplace=True)
 
-    return df
+def total_area(df):
+
+    return df["Area"].sum()
+
+
+def average_yield(df):
+
+    return df["Yield"].mean()
+
+
+def total_states(df):
+
+    return df["State"].nunique()
+
+
+def total_crops(df):
+
+    return df["Crop"].nunique()
